eaef5041aeef7ab870d39a384d2e3cd29cc7d322
worknai009 <worknai009@gmail.com>
2026-01-03 15:31:13 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,9 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'http://localhost:5173'
+  'http://localhost:5173',
+  'https://worknaihrms.online',
+  'http://worknaihrms.online'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
