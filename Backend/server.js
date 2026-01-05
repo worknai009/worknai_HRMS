af953be7928cdb0087db9ce2eb47e050443c5264
sourabh-dhangar <sourabhdhangar187@gmail.com>
2026-01-05 14:39:53 +0530

    Update server file  text

--- Backend/server.js
+++ Backend/server.js
@@ -110,7 +110,7 @@ app.use((err, req, res, next) => {
 });
 
 /* ================= START SERVER ================= */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
